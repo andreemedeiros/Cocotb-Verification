a42fc3761f1dc87fc726593c88f16aa539d44c14
André Medeiros <andre.escariao1@gmail.com>
2024-03-24 02:01:08 -0300

    Add files via upload

---
+++ Basic/run.sh
@@ -0,0 +1,10 @@
+cd /home/runner
+export PATH=/usr/bin:/bin:/tool/pandora64/bin:/usr/share/iverilog-0.9.7/bin
+export PYTHONPATH=/playground_lib/cocotb-1.2/build/libs/x86_64:/playground_lib/cocotb-1.2:.
+export MODULE=testbench
+export LD_LIBRARY_PATH=/playground_lib/cocotb-1.2/build/libs/x86_64
+export COCOTB_SIM=1
+export COCOTB_HOME=/playground_lib/cocotb-1.2
+export COCOTB_LIB=/playground_lib/cocotb-1.2/build/libs/x86_64
+export HOME=/home/runner
+iverilog '-Wall' design.sv  && unbuffer vvp -M $COCOTB_LIB -m gpivpi a.out  ; echo 'Creating result.zip...' && zip -r /tmp/tmp_zip_file_123play.zip . && mv /tmp/tmp_zip_file_123play.zip result.zip
